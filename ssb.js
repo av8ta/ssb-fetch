4ee9d67f3ee22b6a1fcdfb27f6dca2ed57ef3b53
av8ta <av8ta@protonmail.com>
2021-12-30 11:07:09 +0800

    add ssb configuration

--- ssb.js
+++ ssb.js
@@ -1,16 +1,19 @@
 const Client = require('ssb-client')
+const Config = require('ssb-config/inject')
+const debug = require('debug')('ssb-fetch')
 
-module.exports = options =>
+module.exports = (options = {}) =>
   new Promise((resolve, reject) => {
-    Client(options, (error, sbot) => {
+    const appname = options.appname || process.env.ssb_appname || 'ssb'
+    options.appname = appname
+    process.env.ssb_appname = appname
+    const config = Config(appname, options)
+    const keys = config.keys
+    keys.private = null
+    debug({ ...config, keys })
+
+    Client(config, (error, sbot) => {
       if (error) reject(error)
       else resolve(sbot)
     })
   })
-
-// module.exports = new Promise((resolve, reject) => {
-//   Client((error, sbot) => {
-//     if (error) reject(error)
-//     else resolve(sbot)
-//   })
-// })
